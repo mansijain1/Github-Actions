bcf875f712d46de69bf0e7ed0c4ab0a1efcbb39b
mansijain1knoldus <126751871+mansijain1knoldus@users.noreply.github.com>
2023-12-06 16:11:52 +0530

    Update requested-session_api.js

--- go1percent/test/Go1PercentAPIAutomation/RequestedSession/requested-session_api.js
+++ go1percent/test/Go1PercentAPIAutomation/RequestedSession/requested-session_api.js
@@ -20,7 +20,13 @@ describe('api testing', function () {
     const header = globals.admin.headers;
     const tokenHeaders = globals.admin.tokenHeaders;
     const tokenBody = globals.admin.tokenBody;
-
+  
+    const commonExpectation = (startTimestamp, response) => {
+      const endTimestamp = Date.now(); // Record the end time
+      const responseTime = endTimestamp - startTimestamp; // Calculate response time in milliseconds
+      console.log("time" + responseTime);
+      expect(responseTime).to.be.below(2000); //Response time assertion
+  }
     it('get api token', async function ({ supertest }) {
         await supertest
             .request("https://auth.go1percent.com/auth/realms/nashtech/protocol/openid-connect")
@@ -36,120 +42,92 @@ describe('api testing', function () {
     });
 
     it('admin should able to see the available requested sessions', async function ({ supertest }) {
-      const startTime = performance.now();
-  
+      const startTime = Date.now();  
       const response = await supertest
         .request(baseUrl)
         .get(`/v02/sessions/manage?pageNumber=${globals.queryRequested.pageNumber}&pageSize=${globals.queryRequested.pageSize}&filter=${globals.queryRequested.filter}&search=${globals.queryRequested.search}`)
-        .set(globals)
-        // .set('Authorization', accessToken)
-  
+        .set(header)
         .expect(200)
         .expect('Content-Type', 'application/json')
         .then((response)=>{
-          console.log(response.body)
-          const endTime = performance.now();
-          const responseTime = endTime - startTime
-          expect(responseTime).to.be.lessThan(2000)
           expect(response.body).to.have.property('sessions').that.is.an('array');
+          commonExpectation(startTime, response);
         });
     });
 
 
     it('admin should able to add tags', async function ({ supertest }) {
-      const startTime = performance.now();
-
+      const startTime = Date.now();
       const response = await supertest
-        .request(headers.base_url)
+        .request(baseUrl)
         .put(`/v02/sessions/manage/update/${sessionID}`)
-        .set('source', headers.source)
-        .set('Authorization', accessToken)
+        .set(header)
         .send(addTag)
-  
         .expect(200)
         .expect('Content-Type', 'application/json')
         .then((response)=>{
-          console.log(response.body)
-          const endTime = performance.now();
-          const responseTime = endTime - startTime
-          expect(responseTime).to.be.lessThan(5000);
           expect(response.body.sessionID).to.be.not.empty;
           expect(response.body).to.have.property('sessionTag').that.is.an('array');;
           expect(response.body.sessionTag).to.equal(addTag.sessionTag);
           expect(response.body.sessionTag).to.have.lengthOf.at.least(1);
+          commonExpectation(startTime, response);
         });
     });
 
 
     it('admin should able to remove tags', async function ({ supertest }) {
-      const startTime = performance.now();
+      const startTime = Date.now();
 
       const response = await supertest
-        .request(headers.base_url)
+        .request(baseUrl)
         .put(`/v02/sessions/manage/update/${sessionID}`)
-        .set('source', headers.source)
-        .set('Authorization', accessToken)
+        .set(header)
         .send(removeTag)
-
         .expect(200)
         .expect('Content-Type', 'application/json')
         .then((response)=>{
-          console.log(response.body)
-          const endTime = performance.now();
-          const responseTime = endTime - startTime
-          expect(responseTime).to.be.lessThan(5000);
           expect(response.body.sessionID).to.be.not.empty;
           expect(response.body.sessionTag).to.equal(removeTag.sessionTag);
           expect(response.body.sessionTag).to.be.empty;
+          commonExpectation(startTime, response);
         });
   });
 
 
   it('admin should able to approve the session with feedback form', async function({supertest}) {
-    const startTime = performance.now();
+    const startTime = Date.now();
 
     const response = await supertest
-      .request(headers.base_url)  
+      .request(baseUrl)  
       .put(`/v02/sessions/manage/approve/requested/${sessionID}`)
-      .set('source',headers.source)
-      .set('Authorization',accessToken)
-
+      .set(header)
       .expect(200)
       .expect('Content-Type', 'application/json')
       .then((response)=>{
-        console.log(response.body)
-        const endTime = performance.now();
-        const responseTime = endTime - startTime
-        expect(responseTime).to.be.lessThan(5000);
         expect(response.body.sessionID).to.be.not.empty;
         expect(response.body).to.have.property('feedbackForm');
+        commonExpectation(startTime, response);
       });
   });
 
 
   it('Verify that admin should able to update the title ', async function({ supertest }) {
-    const startTime = performance.now();
+    const startTime = Date.now();
 
     const response = await supertest
-      .request(headers.base_url)  
+      .request(baseUrl)  
       .put(`/v02/sessions/manage/update/${sessionID}`)
-      .set('source',headers.source)
-      .set('Authorization', accessToken)
+      .set(header)
       .send(title)
-
       .expect(200)
       .expect('Content-Type', 'application/json')
       .expect('Content-Length', '42')
       .then((response)=>{
-        console.log(response.body)
-        const endTime = performance.now();
-        const responseTime = endTime - startTime
-        expect(responseTime).to.be.lessThan(2000);
         expect(response.body.sessionID).to.be.not.empty;
         expect(response.body.topic).to.equal(title.topic);
         expect(response.body).to.have.property('message');
         expect(response.body.message).to.equal('Successfully updated');
-
+        commonExpectation(startTime, response);
       });
   });
 
