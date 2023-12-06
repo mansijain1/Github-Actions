4481eb8558d571e1a2ef8310449f831fccd03d67
mansijain1knoldus <126751871+mansijain1knoldus@users.noreply.github.com>
2023-12-06 12:39:53 +0530

    Update nightwatch.conf.js

--- go1percent/nightwatch.conf.js
+++ go1percent/nightwatch.conf.js
@@ -16,12 +16,12 @@ require('dotenv').config();
 module.exports = {
   // An array of folders (excluding subfolders) where your tests are located;
   // if this is not specified, the test source must be passed as the second argument to the test runner.
-  src_folders: ['test'],
+  src_folders: ['./test'],
   skip_testcases_on_fail: false,
 
   // See https://nightwatchjs.org/guide/concepts/page-object-model.html
 
-  page_objects_path: ['page-objects'],
+  page_objects_path: ['./page-objects'],
 
   // See https://nightwatchjs.org/guide/extending-nightwatch/adding-custom-commands.html
   //custom_commands_path: ['node_modules/nightwatch/examples/custom-commands/'],
