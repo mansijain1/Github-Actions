78f0fa251fda22c40e69147cf8dea433fd1a320e
mansijain1knoldus <126751871+mansijain1knoldus@users.noreply.github.com>
2023-12-06 12:36:29 +0530

    Update nightwatch.conf.js

--- go1percent/nightwatch.conf.js
+++ go1percent/nightwatch.conf.js
@@ -21,7 +21,7 @@ module.exports = {
 
   // See https://nightwatchjs.org/guide/concepts/page-object-model.html
 
-  page_objects_path: ['page-objects', './page-objects/Manage_upcoming_sessions', './page-objects/TicketAssignedToMe','page-objects/My_Profile', 'page-objects/knolxSessions'],
+  page_objects_path: ['page-objects'],
 
   // See https://nightwatchjs.org/guide/extending-nightwatch/adding-custom-commands.html
   //custom_commands_path: ['node_modules/nightwatch/examples/custom-commands/'],
